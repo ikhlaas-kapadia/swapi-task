3762c34e1cf81a929f6cc4363e8f751cae77580a
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-08-12 22:15:37 +0100

    create function to fetch data from api

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
+    function fetchData(){
+        $baseUrl = "https://swapi.dev/api/people";
+        $data = file_get_contents($baseUrl);
+        $dataFormatted = json_decode($data, true);
+        $content = $dataFormatted["results"];
+        $moreData = $dataFormatted["next"];
+        $allPeople = $content;
+
+      while($moreData == true) {
+        $baseUrl = $moreData;
+        $data = file_get_contents($baseUrl);
+        $dataFormatted = json_decode($data, true);
+        $moreContent = $dataFormatted["results"];
+        $moreData = $dataFormatted["next"];
+        $allPeople = array_merge($allPeople, $moreContent);
+
+      }
+        // $allPeople = $content;
+        // echo("------>");
+        // print_r ($allPeople);
+        echo count($allPeople);
+        return $allPeople;
+     
+    }
+    $characters = fetchData();
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -12,6 +35,11 @@
     <title>Star Wars</title>
 </head>
 <body>
-    
+    <ul>
+      <?php
+         foreach ($characters as $character) {?>
+         <li> <?php echo $character["name"]; ?> </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
