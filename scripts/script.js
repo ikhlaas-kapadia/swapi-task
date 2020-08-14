2684ee148b5171b707d34ed912e37ae3620e3665
Ikhlaas Kapadia <ikhlaas.ka@gmail.com>
2020-08-14 15:53:17 +0100

    add function to toggle download and reset buttons

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   console.log("ready!");
 
   let selectionCount = 0;
+
+  //highlight selection
+  let characterName = $(".char-name");
   function highlight() {
     if ($(this).css("background-color") === "rgba(0, 0, 0, 0)") {
       $(this).css({ background: "#35b335" });
@@ -10,8 +13,22 @@ $(document).ready(function () {
       $(this).css({ background: "none" });
       selectionCount--;
     }
+    toggleButtons();
+  }
+  //toggle buttons based on selection
+  function toggleButtons() {
+    let buttons = $(".btn-container");
+    let message = $(".selected-text");
+    if (selectionCount >= 3) {
+      console.log(buttons);
+      buttons.removeClass("invisible");
+      message.removeClass("invisible");
+    } else {
+      buttons.addClass("invisible");
+      message.addClass("invisible");
+    }
   }
 
-  let characterName = $(".char-name");
+  //Add highlight event on click of character
   characterName.on("click", highlight);
 });
